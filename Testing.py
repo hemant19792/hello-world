4a11c26cd19929d845c46c0782e4d2046dc401be
hemant19792 <hemant19792@gmail.com>
2017-03-30 14:47:16 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,5 +1,6 @@
 Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
+#Testing the git with some python code
 >>> """Example of Python client calling Knowledge Graph Search API."""
 import json
 import urllib
